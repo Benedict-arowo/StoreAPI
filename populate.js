a249c392d960ac6786aad199e7c170c4b3161dd7
= <=>
2022-11-07 12:25:42 +0300

    Added numeric filters, and patch method for products.

--- populate.js
+++ populate.js
@@ -1,18 +1,19 @@
-require('./db/connectDB')
-const data = require('./products.json')
-const productsModel = require('./models/product')
+require('./db/connectDB');
+const data = require('./products.json');
+const productsModel = require('./models/product');
 
 // For prepopulating the api with data.
 const start = async () => {
   try {
-    await productsModel.deleteMany({}) // Deletes all the currert data in the database
-    await productsModel.create(data) // Populates the database.
-    console.log('Sucessfully poupulated the database!')
-    process.exit(0)
+    await productsModel.deleteMany({}); // Deletes all the currert data in the database
+    await productsModel.create(data); // Populates the database.
+    console.log('Sucessfully poupulated the database!');
+    process.exit(0);
   } catch (error) {
-    console.log(error)
-    process.exit(1)
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
+// Setup args for deleting database before populating
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const { getProducts, getProduct, createProduct, deleteProduct } = require('../controllers/products')
-const Router = express.Router()
+const express = require('express');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  editProduct
+} = require('../controllers/products');
 
-Router.route('/').get(getProducts).post(createProduct)
-Router.route('/:productID').get(getProduct).delete(deleteProduct)
+const Router = express.Router();
 
-module.exports = Router
+Router.route('/').get(getProducts).post(createProduct);
+Router.route('/:productID')
+  .get(getProduct)
+  .delete(deleteProduct)
+  .patch(editProduct);
+
+module.exports = Router;
